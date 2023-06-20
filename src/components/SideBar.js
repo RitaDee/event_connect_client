c93aef5e5e6a51bc9f24598cdfb98c18c92611d5
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-20 06:53:43 +0200

    Add SideBar.js

---
+++ src/components/SideBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../images/logo.png';
+
+const SideBar = () => (
+  <nav className="sidebar">
+    <div className="sidebar-header">
+      <div className="sidebar-header-icon">
+        <img className="logo" src={logo} alt="Our logo" />
+      </div>
+    </div>
+    <ul className="sidebar-list">
+      <li className="sidebar-item">
+        <Link to="/main" className="link">EVENTS</Link>
+      </li>
+      <li className="sidebar-item">
+        <Link to="/add_event" className="link">ADD EVENT</Link>
+      </li>
+      <li className="sidebar-item">
+        <Link to="/eventss/delete" className="link">DELETE EVENT</Link>
+      </li>
+      <li className="sidebar-item">
+        <Link to="/reserve" className="link">RESERVE</Link>
+      </li>
+      <li className="sidebar-item">
+        <Link to="/reservations" className="link">MY RESERVATIONS</Link>
+      </li>
+    </ul>
+    <Link to="/signin" className="link sign-out-side">Sign Out</Link>
+  </nav>
+);
+
+export default SideBar;
