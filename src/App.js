09f52df4b91f19d6a575b376e328e6f963fe00f8
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-20 09:37:24 +0200

    Add Sidebar.js, Main.js, Header.js, assets folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import './App.css';
+/* eslint-disable import/no-extraneous-dependencies */
+// /* eslint-disable import/no-extraneous-dependencies */
+// import { Routes, Route } from 'react-router-dom';
+// import Main from './components/Main';
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<Main />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+// import './App.css';
+// import {
+//   Routes, Route,
+// } from 'react-router-dom';
+// import SplashPage from './pages/SplashPage';
+// import SignUpPage from './pages/SignUpPage';
+// import LoginPage from './pages/LoginPage';
+// import DummyHome from './pages/DummyHome'
+
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<SplashPage />} />
+//       <Route path="/signup" element={<SignUpPage />} />
+//       <Route path="/login" element={<LoginPage />} />
+//       <Route path="/dummyhome" element={<DummyHome />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </Router>
   );
 }
 
