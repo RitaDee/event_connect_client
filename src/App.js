bde6dad0160b44f21e50cdff099e5fa7739dc328
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-20 14:50:06 +0100

    Add event detail page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// /* eslint-disable import/no-extraneous-dependencies */
-// import { Routes, Route } from 'react-router-dom';
-// import Main from './components/Main';
-
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Main />} />
-//     </Routes>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
+import EventDetails from './pages/events/EventDetailsPages';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/events/:id" element={<EventDetails />} />
       </Routes>
     </Router>
   );
