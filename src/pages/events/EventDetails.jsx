99540c4fbff5ce35f7ec35a7295fbedb5aea5fa1
Rita Nkem Daniel <ritankemdaniel@gmail.com>
2023-06-21 14:11:55 +0100

    Modify EventDetails file

--- src/pages/events/EventDetails.jsx
+++ src/pages/events/EventDetails.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
+import {
+  Card, Flex, Heading, Image,
+} from '@chakra-ui/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchEventDetail } from '../../redux/slice/eventDetailSlice';
+import bananaImage from '../../images/banana.png';
 
 const EventDetails = () => {
   const { id } = useParams();
   const event = useSelector((state) => state.eventDetail.event);
-  // const isLoading = useSelector((state) => state.eventDetail.loading);
-  // const error = useSelector((state) => state.eventDetail.error);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -16,21 +17,45 @@ const EventDetails = () => {
   }, [dispatch, id]);
 
   return (
-    <div>
-      <h2 className="event_title">{event?.title}</h2>
-      <p className="event_date">
-        Date:
-        {event?.date}
-      </p>
-      <p className="event_time">
-        Time:
-        {event?.time}
-      </p>
-      <p className="event_description">
-        Description:
-        {event?.description}
-      </p>
-    </div>
+    <Flex
+      justify="space-around"
+      alignItems="center"
+      p="4"
+    >
+      <Card
+        pt={180}
+      >
+        <Image width={500} src={bananaImage} />
+      </Card>
+
+      <Flex
+        flexDirection="column"
+        pb={150}
+      >
+        <Heading variant="primary">{event?.title}</Heading>
+        <Flex bg="gray" justify="space-between">
+          Date
+          {' '}
+          <Flex>
+            {event?.date}
+          </Flex>
+        </Flex>
+        <Flex>
+          Time
+          {' '}
+          <Flex>
+            {event?.time}
+          </Flex>
+        </Flex>
+        <Flex bg="gray" justify="space-between">
+          Description
+          {' '}
+          <Flex>
+            {event?.description}
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
 
