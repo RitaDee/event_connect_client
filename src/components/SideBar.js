7ef4a50803176164f59221f41fd02229150bb0db
jugosack <jugoslav_ackoski@yahoo.com>
2023-06-29 13:36:05 +0200

    Add changes in Sidebar.js and style.css for adding hamburger icon

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,69 @@
-import React from 'react';
+// import React from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import logo from '../images/logo.png';
+// import '../styles/style.css';
+// import '../styles/main.css';
+
+// const SideBar = () => {
+//   const navigate = useNavigate();
+
+//   const handleSignOut = async () => {
+//     sessionStorage.removeItem('userId');
+//     sessionStorage.removeItem('token');
+//     navigate('/signin');
+//   };
+
+//   return (
+//     <nav>
+//       <div className="sidebar-header">
+//         <div className="sidebar-header-icon">
+//           <img className="logo" src={logo} alt="Our logo" />
+//         </div>
+//       </div>
+//       <ul className="sidebar-list">
+//         <li className="sidebar-item">
+//           <Link to="/events" className="link">
+//             EVENTS
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/add_event" className="link">
+//             ADD EVENT
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/delete" className="link">
+//             DELETE EVENT
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/reserve" className="link">
+//             RESERVE
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/reservations" className="link">
+//             MY RESERVATIONS
+//           </Link>
+//         </li>
+//         <li className="sidebar-item">
+//           <Link to="/reservations" className="link">
+//             <button
+//               type="button"
+//               className="link sign-out-side"
+//               onClick={handleSignOut}
+//             >
+//               SIGN OUT
+//             </button>
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// };
+
+// export default SideBar;
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import logo from '../images/logo.png';
 import '../styles/style.css';
@@ -6,11 +71,16 @@ import '../styles/main.css';
 
 const SideBar = () => {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleSignOut = async () => {
     sessionStorage.removeItem('userId');
     sessionStorage.removeItem('token');
     navigate('/signin');
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
@@ -20,44 +90,51 @@ const SideBar = () => {
           <img className="logo" src={logo} alt="Our logo" />
         </div>
       </div>
-      <ul className="sidebar-list">
+      <ul className={`sidebar-list ${isMenuOpen ? 'open' : ''}`}>
         <li className="sidebar-item">
-          <Link to="/events" className="link">
+          <Link to="/events" className="link" onClick={toggleMenu}>
             EVENTS
           </Link>
         </li>
         <li className="sidebar-item">
-          <Link to="/add_event" className="link">
+          <Link to="/add_event" className="link" onClick={toggleMenu}>
             ADD EVENT
           </Link>
         </li>
         <li className="sidebar-item">
-          <Link to="/delete" className="link">
+          <Link to="/delete" className="link" onClick={toggleMenu}>
             DELETE EVENT
           </Link>
         </li>
         <li className="sidebar-item">
-          <Link to="/reserve" className="link">
+          <Link to="/reserve" className="link" onClick={toggleMenu}>
             RESERVE
           </Link>
         </li>
         <li className="sidebar-item">
-          <Link to="/reservations" className="link">
+          <Link to="/reservations" className="link" onClick={toggleMenu}>
             MY RESERVATIONS
           </Link>
         </li>
         <li className="sidebar-item">
-          <Link to="/reservations" className="link">
-            <button
-              type="button"
-              className="link sign-out-side"
-              onClick={handleSignOut}
-            >
-              SIGN OUT
-            </button>
-          </Link>
+          <button
+            type="button"
+            className="link sign-out-side"
+            onClick={handleSignOut}
+          >
+            SIGN OUT
+          </button>
         </li>
       </ul>
+      <button
+        type="button"
+        className={`hamburger-icon ${isMenuOpen ? 'open' : ''}`}
+        onClick={toggleMenu}
+      >
+        <span className="line" />
+        <span className="line" />
+        <span className="line" />
+      </button>
     </nav>
   );
 };
